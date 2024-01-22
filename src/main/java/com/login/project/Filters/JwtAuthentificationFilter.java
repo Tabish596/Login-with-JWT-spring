2948856f2f17dc478e854db62a66b1ecd4948039
Tabish <tabishakhtarsr@gmail.com>
2024-01-22 12:13:12 +0530

    Added more code inside jwt setup class

---
+++ src/main/java/com/login/project/Filters/JwtAuthentificationFilter.java
@@ -0,0 +1,34 @@
+package com.login.project.Filters;
+
+import com.login.project.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthentificationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authheader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authheader==null || !authheader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authheader.substring(7);
+        userEmail=jwtService.extractUsername(jwt);
+    }
+}
